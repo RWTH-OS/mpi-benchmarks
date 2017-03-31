4bb48cff6e6ec294da332e93615a2fde54ab30e5
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2017-03-31 16:39:38 +0200

    fixed interval for pingpong_ts

--- pingpong_ts.c
+++ pingpong_ts.c
@@ -51,128 +51,136 @@ int main(int argc, char **argv) {
 
 	useconds_t delay = DEFAULTDELAY;
 	uint32_t length = DEFAULTLEN;
-	uint32_t iterations = DEFAULTITER;
-	int32_t numrounds = DEFAULTROUNDS;
-	int32_t round;
+uint32_t iterations = DEFAULTITER;
+int32_t numrounds = DEFAULTROUNDS;
+int32_t round;
 
-	double timer;
-	bool run_infinitely;
-	MPI_Status status;
+double timer;
+bool run_infinitely;
+MPI_Status status;
 
-	/* determine arguments */
-	while ((arg = getopt(argc, argv, "i:r:l:hd:")) != -1) {
-		switch (arg) {
-			case 'r':
-				numrounds = atoi(optarg);
-				break;
-			case 'l':
-				length = atoi(optarg);
-				break;
-			case 'i':
-				iterations = atoi(optarg);
-				break;
-			case 'd':
-				delay = atof(optarg);
-				break;
-			case 'h':
-				printf(
-				    "usage %s [-l message_length (def: %d)] "
-				    "[-i iterations (def: %d)] "
-				    "[-d delay in us (def: %f)] "
-				    "[-r rounds (def: %d)]\n",
-				    argv[0], DEFAULTLEN, DEFAULTITER, DEFAULTDELAY,
-				    DEFAULTROUNDS);
-				exit(0);
-		}
+/* determine arguments */
+while ((arg = getopt(argc, argv, "i:r:l:hd:")) != -1) {
+	switch (arg) {
+		case 'r':
+			numrounds = atoi(optarg);
+			break;
+		case 'l':
+			length = atoi(optarg);
+			break;
+		case 'i':
+			iterations = atoi(optarg);
+			break;
+		case 'd':
+			delay = atof(optarg);
+			break;
+		case 'h':
+			printf(
+			    "usage %s [-l message_length (def: %d)] "
+			    "[-i iterations (def: %d)] "
+			    "[-d delay in us (def: %f)] "
+			    "[-r rounds (def: %d)]\n",
+			    argv[0], DEFAULTLEN, DEFAULTITER, DEFAULTDELAY,
+			    DEFAULTROUNDS);
+			exit(0);
 	}
+}
 
-	/* initialize MPI environment */
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
+/* initialize MPI environment */
+MPI_Init(&argc, &argv);
+MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
 
-	/* check for errors and determine remote rank */
-	if (num_ranks != 2) {
-		if (my_rank == 0)
-			fprintf(stderr, "%s needs exactly two UEs; try again\n",
-				argv[0]);
-		exit(-1);
-	}
-	remote_rank = (my_rank + 1) % 2;
+/* check for errors and determine remote rank */
+if (num_ranks != 2) {
+	if (my_rank == 0)
+		fprintf(stderr, "%s needs exactly two UEs; try again\n",
+			argv[0]);
+	exit(-1);
+}
+remote_rank = (my_rank + 1) % 2;
 
 /* perform a warm-up of the cache */
 #ifdef _CACHE_WARM_UP_
-	for (i = 0; i < length; i++) {
-		/* cache warm-up: */
-		dummy += send_buffer[i];
-		dummy += recv_buffer[i];
-	}
+for (i = 0; i < length; i++) {
+	/* cache warm-up: */
+	dummy += send_buffer[i];
+	dummy += recv_buffer[i];
+}
 #endif
 
-	/* check for infinite test */
+/* check for infinite test */
+if (numrounds == -1) {
+	run_infinitely = true;
+} else {
+	run_infinitely = false;
+}
+
+if (my_rank == 0) {
+	printf("Starting the benchmark:\n");
 	if (numrounds == -1) {
-		run_infinitely = true;
+		printf("Rounds     :        inf\n");
 	} else {
-		run_infinitely = false;
+		printf("Rounds     : %10d\n", numrounds);
 	}
+	printf("Iterations : %10d\n", iterations);
+	printf("Msg Length : %10d\n", length);
+}
 
-	if (my_rank == 0) {
-		printf("Starting the benchmark:\n");
-		if (numrounds == -1) {
-			printf("Rounds     :        inf\n");
-		} else {
-			printf("Rounds     : %10d\n", numrounds);
-		}
-		printf("Iterations : %10d\n", iterations);
-		printf("Msg Length : %10d\n", length);
+/* synchronize and start the PingPong */
+MPI_Barrier(MPI_COMM_WORLD);
+if (my_rank == 0) {
+	for (i = 0; i < WARMUPITER; ++i) {
+		MPI_Send(send_buffer, length, MPI_CHAR, remote_rank, 0,
+			 MPI_COMM_WORLD);
+		MPI_Recv(recv_buffer, length, MPI_CHAR, remote_rank, 0,
+			 MPI_COMM_WORLD, &status);
 	}
-
-	/* synchronize and start the PingPong */
 	MPI_Barrier(MPI_COMM_WORLD);
-	if (my_rank == 0) {
-		for (i = 0; i < WARMUPITER; ++i) {
-			MPI_Send(send_buffer, length, MPI_CHAR, remote_rank, 0,
-				 MPI_COMM_WORLD);
-			MPI_Recv(recv_buffer, length, MPI_CHAR, remote_rank, 0,
-				 MPI_COMM_WORLD, &status);
+
+	for (round = 0; run_infinitely || (round < numrounds);
+	     ++round) {
+		/* start timer: */
+		timer = MPI_Wtime();
+
+		for (i = 0; i < iterations; ++i) {
+			MPI_Send(send_buffer, length, MPI_CHAR,
+				 remote_rank, 0, MPI_COMM_WORLD);
+			MPI_Recv(recv_buffer, length, MPI_CHAR,
+				 remote_rank, 0, MPI_COMM_WORLD,
+				 &status);
 		}
-		MPI_Barrier(MPI_COMM_WORLD);
 
-		for (round = 0; run_infinitely || (round < numrounds);
-		     ++round) {
-			/* start timer: */
-			timer = MPI_Wtime();
+		/* stop timer: */
+		timer = (MPI_Wtime() - timer);
 
-			for (i = 0; i < iterations; ++i) {
-				MPI_Send(send_buffer, length, MPI_CHAR,
-					 remote_rank, 0, MPI_COMM_WORLD);
-				MPI_Recv(recv_buffer, length, MPI_CHAR,
-					 remote_rank, 0, MPI_COMM_WORLD,
-					 &status);
-			}
+		printf("%1.2lf\n",
+		       timer / (2.0 * iterations) * 1000000);
+		fflush(stdout);
 
-			/* stop timer: */
-			timer = (MPI_Wtime() - timer);
-
-			printf("%1.2lf\n",
-			       timer / (2.0 * iterations) * 1000000);
-			fflush(stdout);
-
-			// delay next ping
-			usleep(delay);
+		// delay next ping
+		useconds_t timer_usec = (useconds_t)(timer *1e6);
+		useconds_t sleep_time;
+		if (delay < timer_usec) {
+			sleep_time = 0;
+		} else {
+			sleep_time = delay - timer_usec;
 		}
-	} else {
-		for (i = 0; i < WARMUPITER; ++i) {
-			MPI_Recv(recv_buffer, length, MPI_CHAR, remote_rank, 0,
-				 MPI_COMM_WORLD, &status);
-			MPI_Send(send_buffer, length, MPI_CHAR, remote_rank, 0,
-				 MPI_COMM_WORLD);
-		}
-		MPI_Barrier(MPI_COMM_WORLD);
 
-		for (round = 0; run_infinitely || (round < numrounds);
-		     ++round) {
-			for (i = 0; i < iterations; ++i) {
+		usleep(sleep_time);
+	}
+} else {
+	for (i = 0; i < WARMUPITER; ++i) {
+		MPI_Recv(recv_buffer, length, MPI_CHAR, remote_rank, 0,
+			 MPI_COMM_WORLD, &status);
+		MPI_Send(send_buffer, length, MPI_CHAR, remote_rank, 0,
+			 MPI_COMM_WORLD);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	for (round = 0; run_infinitely || (round < numrounds);
+	     ++round) {
+		for (i = 0; i < iterations; ++i) {
 				MPI_Recv(recv_buffer, length, MPI_CHAR,
 					 remote_rank, 0, MPI_COMM_WORLD,
 					 &status);
